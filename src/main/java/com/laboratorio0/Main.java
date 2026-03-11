15ea061cc9ed1635c4a5f4a286835858c4b82a89
Elise <e260299@dac.unicamp.br>
2026-03-10 22:36:53 -0300

    added ana

--- src/main/java/com/laboratorio0/Main.java
+++ src/main/java/com/laboratorio0/Main.java
@@ -2,7 +2,7 @@ package com.laboratorio0;
 
 public class Main {
     public static void main(String[] args) {
-        Usuario u1 = new Usuario("", "ana@email.com");
+        Usuario u1 = new Usuario("Ana", "ana@email.com");
         Usuario u2 = new Usuario("Carlos", "carlos@email.com"); // Perfil do chefe
 
         Chefe chefe = new Chefe(u2);
